bc0cd09874587128c0e12dc04ff20b7be39a8f70
daiagi <daiagi@gmail.com>
2024-06-01 01:30:21 +0300

    change swagger page title

--- src/server/swaggerConfig.ts
+++ src/server/swaggerConfig.ts
@@ -4,8 +4,12 @@ import { Express } from "express";
 
 const swaggerDocument = YAML.load("./swagger.yaml");
 
+const options: swaggerUi.SwaggerOptions = {
+  customSiteTitle: "Polkadot Light Client API Documentation",
+  
+};
 export const setupSwagger = (app: Express) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 };
 
 export default setupSwagger;
