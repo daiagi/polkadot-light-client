58c5e6818253ca6ce0ef674f8563f70a9cc3f143
daiagi <daiagi@gmail.com>
2024-06-01 00:42:39 +0300

    better utils tests

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,24 +1,35 @@
-import { pad } from "../src/utils/arrays";
+import { chunkArray, pad } from "../src/utils/arrays";
 import { concatenateHashes, hash } from "../src/utils/hash";
 
 describe("utils test", () => {
   it("hash sanity check", () => {
     const data = "some string";
-    expect(hash(data)).toBe(
-      "61d034473102d7dac305902770471fd50f4c5b26f6831a56dd90b5184b3c30fc"
-    );
+    const data2 = "some string ";
+    expect(hash(data)).not.toBe(hash(data2));
   });
 
-  it("hash empty string", () => {
-    const data = "";
-    expect(hash(data)).toBe(
-      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    );
-  });
 
   it("concatenates hashes correctly", () => {
     const leftHash = "00ff0a1b";
     const rightHash = "01020304";
+
+    const expectedConcatenatedHash = hash(
+      Buffer.concat([Buffer.from([0, 255, 10, 27]), Buffer.from([1, 2, 3, 4])])
+    );
+    const revereExpectedConcatenatedHash = hash(
+      Buffer.concat([Buffer.from([1, 2, 3, 4]), Buffer.from([0, 255, 10, 27])])
+    );
+
+    expect(concatenateHashes(leftHash, rightHash)).toBe(
+      expectedConcatenatedHash
+    );
+    expect(concatenateHashes(rightHash, leftHash)).toBe(
+      revereExpectedConcatenatedHash
+    );
+  });
+  it("concatenates hashes correctly2", () => {
+    const leftHash = "0x00ff0a1b";
+    const rightHash = "0x01020304";
 
     const expectedConcatenatedHash = hash(
       Buffer.concat([Buffer.from([0, 255, 10, 27]), Buffer.from([1, 2, 3, 4])])
@@ -58,41 +69,16 @@ describe("utils test", () => {
     ]);
   });
 
-  it("concatenates hashes correctly", () => {
-    const leftHash = "00ff0a1b";
-    const rightHash = "01020304";
+  it("chunks array correctly", () => {
+    const hashes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const chunks = chunkArray(hashes, 2);
+    expect(chunks).toEqual([
+      ["1", "2"],
+      ["3", "4"],
+      ["5", "6"],
+      ["7", "8"],
+      ["9"],
+    ]);
 
-    const expectedConcatenatedHash = hash(
-      Buffer.concat([Buffer.from([0, 255, 10, 27]), Buffer.from([1, 2, 3, 4])])
-    );
-
-    expect(concatenateHashes(leftHash, rightHash)).toBe(
-      expectedConcatenatedHash
-    );
-  });
-
-  it("not semtrical to order", () => {
-    const leftHash = "00ff0a1b";
-    const rightHash = "01020304";
-
-    const expectedConcatenatedHash = hash(
-      Buffer.concat([Buffer.from([0, 255, 10, 27]), Buffer.from([1, 2, 3, 4])])
-    );
-
-    expect(concatenateHashes(leftHash, rightHash)).toBe(
-      expectedConcatenatedHash
-    );
-    expect(concatenateHashes(rightHash, leftHash)).not.toBe(
-      expectedConcatenatedHash
-    );
-
-    expect(concatenateHashes(rightHash, leftHash)).toBe(
-      hash(
-        Buffer.concat([
-          Buffer.from([1, 2, 3, 4]),
-          Buffer.from([0, 255, 10, 27]),
-        ])
-      )
-    );
   });
 });
